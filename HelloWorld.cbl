6b29367dd36de0f6aa4075ef2908634c68aa06c3
ThibautLemichel <thibaut.lemichel@student.junia.com>
2024-03-28 09:53:35 +0100

    update Cobol

--- HelloWorld.cbl
+++ HelloWorld.cbl
@@ -1,5 +1,17 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLOWORLD.
+
+       DATA DIVISION. 
+       WORKING-STORAGE SECTION. 
+       01 VAR1       PIC 9(3).
+          88 MAJEUR           VALUE 18.
+
        PROCEDURE DIVISION.
-           DISPLAY "HELLO WORLD".
+
+           DISPLAY "Entrer valeur a :".
+
+           ACCEPT VAR1.
+           DISPLAY "Valeur de a : " VAR1.
+           DISPLAY "Est majeur ? " MAJEUR.
+
            STOP RUN.
